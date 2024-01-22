3d07fe62dfd9ba90137cfa9bc1bb29c66ad7d0f0
Shreyakumariv <shreyakumariv@gmail.com>
2024-01-22 12:19:32 +0530

    The user directory application successfully combines simplicity with functionality, providing users with an efficient and enjoyable way to explore user profiles and associated posts. The integration of a clock and country selector adds a personalized touch to the user experience. Overall, the project demonstrates effective use of React and related technologies to create a user-friendly application.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Directory from './Components/JS/Directory';
 import ProfilePage from './Components/JS/ProfilePage';
-import Test from './Components/JS/Test';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/test' element={<Test />} />
         <Route path='/' element={<Directory />} />
         <Route path='/profile/:id' element={<ProfilePage />} />
       </Routes>
